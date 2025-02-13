588a19ef420f3aec4c34dd76383abd3d05ef3874
Tannsxerd <tansxerd1231@gmail.com>
2025-02-13 15:19:31 +0700

    user story

---
+++ functionaltest.py
@@ -0,0 +1,11 @@
+# Create your tests here.
+#tanny find poll web
+#title of web is poll 
+#tanny find text box
+#tanny fill text box
+#tanny find button
+#tanny press button
+#poll topic is show
+#tanny find vote choice 
+#tanny select vote choice 
+#tanny save vote
