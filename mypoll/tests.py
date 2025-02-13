e2b559ffd01cd53a9ca33e00b12a389d63df854c
Tannsxerd <tansxerd1231@gmail.com>
2025-02-13 16:00:58 +0700

    add create question unit Test

--- mypoll/tests.py
+++ mypoll/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
 
-# Create your tests here.
+class QuestionModelTests(TestCase):
+    def test_create_question(self):
+        question = Question.objects.create(question_text ="thai food" pub_date = timezone)
+         
